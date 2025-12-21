441cac4237d8f21c213c65f014ae1dbbbb3785ca
matthewnoahkim <mattnoahkim@gmail.com>
2025-12-20 21:00:59 -0800

    oauth

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -14,6 +14,7 @@ import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 import { documentsRouter } from './routes/documents';
 import { partsRouter } from './routes/parts';
 import { sketchesRouter } from './routes/sketches';
